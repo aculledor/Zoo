adee6804e450ad2fc03795a256afd6953ec29622
Abraham <aculledor@gmail.com>
2020-04-25 18:03:32 +0200

    Cambio para Lois

---
+++ src/parteGrafica/EjemploClase_2.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parteGrafica;
+
+/**
+ *
+ * @author acull
+ */
+public class EjemploClase_2 {
+    //Comentario de prueba: Hola
+}
